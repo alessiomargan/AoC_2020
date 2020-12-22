027062ee2967a702b83f4f9528799c52fab7d135
Alessio Margan <alessio.margan@iit.it>
2020-12-22 01:52:11 +0100

    End day 3 ... stuck on len(line) new_line when reading input file because test lines do not have them

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ from collections import Counter, defaultdict, namedtuple, deque, abc, OrderedDic
 from functools import lru_cache
 from statistics import mean, median, mode, stdev, variance
 from itertools import (permutations, combinations, chain, cycle, product, islice,
-                       takewhile, zip_longest, count as count_from)
+                       takewhile, zip_longest, count as count_from, tee)    
 from heapq import heappop, heappush
 from numba import jit
 from dataclasses import dataclass
